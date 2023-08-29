708c32aec7ca7f59fa1aeab52d94f7d40cce178e
Petr Barabáš <petr.barabas@memos.cz>
2023-08-29 17:00:23 +0200

    Added Entity SQL Server

--- DeveloperTest/ToDoDbContext.cs
+++ DeveloperTest/ToDoDbContext.cs
@@ -1,19 +1,29 @@
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
 
 namespace DeveloperTest;
 
 public class ToDoDbContext : DbContext
 {
-    //public DbSet<Student> Students { get; set; }
-    //public DbSet<Course> Courses { get; set; }
+    private readonly IConfiguration _configuration;
 
-    protected ToDoDbContext()
+    protected ToDoDbContext(IConfiguration configuration)
     {
-
+        _configuration = configuration;
     }
 
     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
     {
+        string connectionString = _configuration.GetConnectionString("connection")!;
+
+        if (String.IsNullOrEmpty(connectionString))
+        {
+            throw new ConnectionIsNotConfigured();
+        }
+
+
+        optionsBuilder.UseSqlServer(connectionString)
+            .EnableSensitiveDataLogging();
     }
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
