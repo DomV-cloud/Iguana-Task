be9fc39749db1fa0d10130fbd68a7c33c6416395
Petr Barabáš <petr.barabas@memos.cz>
2023-08-29 17:23:32 +0200

    Added methods in interface

--- DeveloperTest/Repository/Interfaces/IRepository.cs
+++ DeveloperTest/Repository/Interfaces/IRepository.cs
@@ -3,10 +3,16 @@
     public interface IRepository<T> where T : EntityBase
     {
         /*
+         * 
          get collection of unpaid invoices
          pay an invoice (changing status to paid)
          edit an invoice (PATCH request)
          
          */
+
+
+        public List<T> GetAllInvoices();
+        public T PayInvoice(int id);
+        public T EditInvoice(int id);
     }
 }
