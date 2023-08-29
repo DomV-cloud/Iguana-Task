17f8adbb5106e90173eac0513234a0a97c0c76f7
Petr Barabáš <petr.barabas@memos.cz>
2023-08-29 17:01:16 +0200

    Created Exceptions folder

---
+++ DeveloperTest/Exceptions/ConnectionIsNotConfigured.cs
@@ -0,0 +1,15 @@
+﻿namespace DeveloperTest.Exceptions
+{
+    public class ConnectionIsNotConfigured: Exception
+    {
+        public ConnectionIsNotConfigured() : base()
+        {
+        }
+        public ConnectionIsNotConfigured(string message) : base(message)
+        {
+        }
+        public ConnectionIsNotConfigured(string message, Exception e) : base(message, e)
+        {
+        }
+    }
+}
