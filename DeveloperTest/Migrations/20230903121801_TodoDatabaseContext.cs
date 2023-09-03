4787957bfa7241c37810a7afd922d4d2e2886075
Petr Barabáš <petr.barabas@memos.cz>
2023-09-03 14:18:11 +0200

    Created Invoice Model

---
+++ DeveloperTest/Migrations/20230903121801_TodoDatabaseContext.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace DeveloperTest.Migrations
+{
+    /// <inheritdoc />
+    public partial class TodoDatabaseContext : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
--- DeveloperTest/Models/Invoice.cs
+++ DeveloperTest/Models/Invoice.cs
@@ -1,10 +1,15 @@
 ﻿using DeveloperTest.Repository;
+using System.ComponentModel.DataAnnotations;
 
 namespace DeveloperTest.Models
 {
     public class Invoice: EntityBase
     {
 
+        [Required]
+        public int Invoice_Number{ get; set; }
 
+
+        public bool IsPaid { get; set; } = false;
     }
 }
